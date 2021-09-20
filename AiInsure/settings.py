cf55ff66fa9e6e181c9eb85dec0eba3e281828b3
vivianwang777 <vivian_wang_7@hotmail.com>
2021-09-20 20:31:38 +1000

    First Commit

--- AiInsure/settings.py
+++ AiInsure/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-#import environ
+import environ
 from pathlib import Path
 
 from django.core.exceptions import ImproperlyConfigured
 
 # Initialise environment variables
-#env = environ.Env()
-#environ.Env.read_env()
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
-#    SECRET_KEY = os.environ["SECRET_KEY"]
-    SECRET_KEY = "my - django - secret - key"
+    SECRET_KEY = os.environ["SECRET_KEY"]
+#    SECRET_KEY = "my - django - secret - key"
 except KeyError:
     raise ImproperlyConfigured("SECRET_KEY environment variable is missing!")
 
